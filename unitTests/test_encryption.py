f56f5032afc60b3c25aab0f860652b9be0f97487
Devin Ramaswami (dpr331) <dpr331@mail.usask.ca>
2020-11-02 18:13:40 -0600

    Removed encryptData function form test file

--- unitTests/test_encryption.py
+++ unitTests/test_encryption.py
@@ -1,28 +1,7 @@
 from cryptography.fernet import Fernet, InvalidToken
+from encryption import encryptData
 import json
 import pytest
-
-
-def encryptData(data, key):
-    """Encrypts the passed in data with the given key
-
-    Takes the data and key passed in and performs a fernet encryption
-    the encrypted data is returned.
-
-    Args:
-        data: A string of unknown format (could be json dump or 
-        normal text string). It should not be encoded.
-        key: The key with which data will be encrypted. The key 
-        type is Fernet
-    Returns:
-        An encoded string containing the Fernet encrypted data. The 
-        only way to access the data is to decrypt it with the same 
-        key that was provided as an argument.
-
-    """
-    cipher_suite = Fernet(key)
-    cipher_text = cipher_suite.encrypt(data.encode())
-    return cipher_text
 
 
 def test_stringEncryption():
