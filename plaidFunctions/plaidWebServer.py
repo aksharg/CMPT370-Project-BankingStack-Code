668468da2ea18b0148008919c92ca68bf627d990
Akshar Gajjar <ang399@usask.ca>
2020-11-26 19:46:22 -0600

    Completed the plaidWebServer.py file. The server works perfectly and opens the link.html file as it's supposed to. The startServer function also returns the plaid_response (contains access_key).

--- plaidFunctions/plaidWebServer.py
+++ plaidFunctions/plaidWebServer.py
@@ -1,65 +1,112 @@
 import json
-import sys
 import mimetypes
-from http.server import BasedHTTPRequestHandler, ThreadingHTTPServer
+import sys
+import webbrowser
+from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 
-class plaidHTTPServer(BasedHTTPRequestHandler):
-    def __init__(self, *args, **kwargs):
-        super.__init__(*args, **kwargs)
-    
-    def fileServe(self, file_path: str):
-        """[summary]
 
-        Args:
-            file_path (str): [description]
-        """
-        pass
-    
-    def send404(self):
-        """[summary]
-        """
-        pass
+class serverDataStore:
+    def __init__(self, server_params):
+        self.server_params = server_params
+        self.plaid_response = None
 
-    def requestPOST(self):
-        """[summary]
-        """
-        pass
+class plaidHTTPServer(BaseHTTPRequestHandler):
+    def __init__(self, server_params: serverDataStore, *args, **kwargs):
+        self.server_params = server_params
+        super().__init__( *args, **kwargs)
 
-    def requestGET(self):
-        """[summary]
-        """
-        pass
+    def fileServe(self, file_path):
+        mime = mimetypes.guess_type(file_path)
+        self.send_response(200)
+        self.send_header('Content-type', mime[0])
+        self.end_headers()
 
-def createPlaidHandler(*args, **kwargs):
+        with open(file_path, 'r') as f:
+            html = f.read()
+
+        html = html.replace('{{SERVER_PARAMS}}',json.dumps(self.server_params.server_params))
+        self.wfile.write(html.encode('utf-8'))
+        self.wfile.flush()
+
+    def send404Error(self):
+        self.send_response(404)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'not found')
+        self.wfile.flush()
+
+    def do_POST(self):
+        path = self.path.split('?')[0]
+        
+        if path == '/api/success':
+            content_length = int(self.headers.get('Content-Length', 0))
+            body = self.rfile.read(content_length)
+
+            self.server_params.plaid_response = json.loads(body)
+            self.server.shutdown()
+            self.server.server_close()
+            return
+
+        else:
+            self.send404Error()
+
+    def do_GET(self):
+        path = self.path.split('?')[0]
+
+        if path == '/link.html':
+            self.fileServe('group9\plaidFunctions\html\link.html')
+            return
+
+        else:
+            self.send404Error()
+
+
+def openWebpage(url):
+    webbrowser.open(url)
+
+def startServer(env: str, clientName: str, token: str, pageTitle: str, accountName: str, type: str):
     """[summary]
+
+    Args:
+        env (str): [description]
+        clientName (str): [description]
+        token (str): [description]
+        pageTitle (str): [description]
+        accountName (str): [description]
+        type (str): [description]
 
     Returns:
         [type]: [description]
     """
-    return plaidHTTPServer(*args, **kwargs)
 
-def serveWebpage(environemt: str, client_name: str, public_token: str, page_title: str, account_name: str, account_type: str) -> Dict:
-    """[summary]
+    server_params = dict(
+        env=env,
+        clientName=clientName,
+        token=token,
+        pageTitle=pageTitle,
+        accountName=accountName,
+        type=type
+    )
 
-    Args:
-        environemt (str): [description]
-        client_name (str): [description]
-        public_token (str): [description]
-        page_title (str): [description]
-        account_name (str): [description]
-        account_type (str): [description]
+    params = serverDataStore(server_params)
 
-    Returns:
-        Dict: [description]
-    """
-    param_json = dict(env=environemt, 
-                      client_name=client_name, 
-                      public_token=public_token, 
-                      page_title=page_title,
-                      account_name=account_name,
-                      account_type=account_type)
-    
-    # with ThreadingHTTPServer(('127.0.0.1',8000), createPlaidHandler) as httpd:
+    def make_handler(*args, **kwargs):
+        return plaidHTTPServer(params, *args, **kwargs)
+
+    with ThreadingHTTPServer(('127.0.0.1', 8000), make_handler) as httpd:
+        host, port = httpd.socket.getsockname()
+
+        openWebpage(f'http://{host}:{port}/link.html')
+
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print('Keyboard interrupt, exiting Server.')
+            sys.exit(0)
+
+    return params.plaid_response
+
 
 if __name__ == '__main__':
-    serveWebpage({})
+    # startServer({})
+    startServer('a','b','c','d','e','f')
